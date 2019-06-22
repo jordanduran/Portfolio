e0eafce0a69b24cd9e90c3943514bd504ba74c0f
Jordan Duran <jordanduranm@gmail.com>
2019-06-22 12:44:59 -0400

    added scroll to top button

--- script.js
+++ script.js
@@ -18,5 +18,16 @@ $(document).ready(function() {
       $("nav").removeClass("black");
     }
   });
+  // scroll to top button
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 40) {
+        $('#topBtn').fadeIn();
+    } else {
+        $('#topBtn').fadeOut();
+    }
+    });
+
+$("#topBtn").click(function() {
+    $('html ,body').animate({scrollTop : 0},800);
 });
-// Scrolling Effect
+});
